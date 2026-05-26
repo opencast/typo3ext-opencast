64c981aaa4429ca759ea8fc563a8f9a9df409a48
Philipp Kitzberger <coding@kitze.net>
2026-05-26 15:18:19 +0200

    Enhance PATH_PATTERNS by 'paella7/ui/'

--- Classes/Helpers/OpencastHelper.php
+++ Classes/Helpers/OpencastHelper.php
@@ -20,6 +20,8 @@ class OpencastHelper extends AbstractOnlineMediaHelper
     private const PATH_PATTERNS = [
         'paella\/ui\/watch\.html\?id=' . self::MEDIA_ID_PATTERN,
         'paella\/ui\/watch\.html\?cid=[0-9a-f]+&id=' . self::MEDIA_ID_PATTERN,
+        'paella7\/ui\/watch\.html\?id=' . self::MEDIA_ID_PATTERN,
+        'paella7\/ui\/watch\.html\?cid=[0-9a-f]+&id=' . self::MEDIA_ID_PATTERN,
         'play\/' . self::MEDIA_ID_PATTERN,
     ];
 
